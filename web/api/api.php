04afefbebb04c9b06fdc99277a52dd45bebca187
Chen Zheng <czheng1968@gmail.com>
2016-04-08 18:24:07 +0000

    Fixed some typos in api.php

--- web/api/api.php
+++ web/api/api.php
@@ -3,14 +3,14 @@ require 'scripts/PocketCore.php'; # This file contains class PocketCore
 
 # This file recieves requests from MCPE servers.
 
-if(isset($_GET['api_key')){
+if(isset($_GET['api_key'])){
   $api = new PocketCore($_GET['api_key']);
   
   if($api->answer){
     die($api->answer); // If connection is made for first time, this will return password for session.
   }
   
-  if(!isset($_GET['pass']){
+  if(!isset($_GET['pass'])){
     die('ERROR: No Password');
   }
   
