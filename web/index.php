3afd14de82c497cca2040ad3e231a0a2ff25246b
Chen Zheng <czheng1968@gmail.com>
2016-04-08 18:28:32 +0000

    Added basic HTML work to index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
-
-die("");
-
-?>
+<html>
+    <head>
+        <title>PocketCore - Home</title>
+        <link rel="stylesheet" href="template/css/materialize.min.css"> <!-- materialize css -->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <!-- google material icons -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- viewport support for mobile -->
+        <meta charset="UTF-8"> <!-- allows utf-8 enocded chars -->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> <!-- jquery -->
+        <script src="template/js/materialize.min.js"></script> <!-- materialize js -->
+    </head>
+    <body>
+        
+    </body>
+</html>
