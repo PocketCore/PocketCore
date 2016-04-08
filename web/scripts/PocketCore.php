35efb76cfb8f313c83040f5a083476096408a346
PrimusLV <k.drivnieks@inbox.lv>
2016-04-08 19:25:08 +0300

    Create PocketCore.php

---
+++ web/scripts/PocketCore.php
@@ -0,0 +1,20 @@
+<?php
+
+class PocketCore 
+{
+
+  public function __construct($api_key){
+    # Check if this server has connected to our server already if not return a password for their session.
+  }
+  
+  /**
+  * @return void
+  */
+  public function request($q, $pass){
+    # 1) Verify password
+    # 2) Decode $q
+    # 3) Verify if $q is valid request.
+    # 4) Do what $q says!
+  }
+}
+?>
